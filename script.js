43a1d13487454defa3347de7cf50be8875ef720b
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2023-11-09 00:01:35 +0530

    update js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 
 var screen = document.querySelector(".screen");
 var btn = document.querySelectorAll(".btn");
+let isResultDisplayed = false;
+let lastResult = null;
+
+function clearScreen() {
+    screen.value = '';
+    isResultDisplayed = false;
+}
 
 for(item of btn){
 item.addEventListener("click",(e)=>{
+    if (isResultDisplayed) {
+        clearScreen();
+    }
+
+    if (lastResult !== null) {
+        screen.value += lastResult; 
+        lastResult = null; 
+    }
+
+    else{
     btn_text = e.target.innerText;
 
     if(btn_text == "⨯"){
@@ -18,22 +35,25 @@ item.addEventListener("click",(e)=>{
         btn_text = "**";
     }
 
+    if(btn_text == '%'){
+        btn_text = "/100";
+    }
+
     screen.value += btn_text;
+    }
 });
 }
 
 function result(){
     try{
         screen.value = eval(screen.value);
+        isResultDisplayed = true;
     }
     catch(error){
         alert("Invalid!");
     }
 }
 
-function clearScreen(){
-    screen.value = "";
-}
 
 function pi(){
     screen.value = Math.PI;
